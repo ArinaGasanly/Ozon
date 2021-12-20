f048070593aa960bb19914d1eed850268cfbdb93
Arina Gasanly <Arinakudinova111@yandex.ru>
2021-12-20 22:52:52 +0300

    добавление 4 урока

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,4 +1,4 @@
-const postData = () => {
+const postData = (cart) => {
 
 /*
 return fetch('http://localhost:3000/goods/24', {
@@ -7,16 +7,9 @@ return fetch('http://localhost:3000/goods/24', {
 .then(res => res.json())
 // Также есть методы Put и Patch
 */
-  return fetch('http://localhost:3000/goods', {
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
-    body: JSON.stringify({
-      id: 27,
-      title: "Ведьмак 3",
-      price: 3000,
-      sale: true,
-      img: "https://cdn1.ozone.ru/multimedia/c400/1023547851.jpg",
-      category: "Игры и софт"
-    }),
+    body: JSON.stringify(cart),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
